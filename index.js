fa48cca0de77188e761897d69e05bf9c3ac45811
Gonzalo Rojas <rojasgonzalo23@gmail.com>
2023-09-25 10:04:24 -0300

    error conexion

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./config/config')
+require('dotenv').config({path: '.env'})
+const path = require('path');
 const Empleado = require('./models/empleado')
 const Turno = require('./models/turnos')
 const Extra = require('./models/extras')
@@ -21,4 +23,26 @@ db.sync()
 app.use(cors())
 
 app.use('/', routes())
-app.listen(5000)
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3001;
+app.listen(port, host, ()=>{
+    console.log('el servidor esta funcionando');
+})
